9d2cff1c715b18db50713d4e5ff2a1e3f4bd82d6
David Durbin <David-Durbin@users.noreply.github.com>
2019-03-25 15:13:14 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ddurbin_interfaceconnection/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.ddurbin_interfaceconnection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn1 = findViewById(R.id.idBtn1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Main2Activity.class));
+            }
+        });
+    }
+}
